c1217f4c99cea4bb9e553779e25be51876b0f64c
NazarKutoviy <147269741+NazarKutoviy@users.noreply.github.com>
2025-06-06 18:46:18 +0300

    фвв

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import debounce from "lodash.debounce";
-import fetchCountries from "./fetchCountries";
 import { alert, notice } from "@pnotify/core";
 import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
 
+// DOM елементи
 const input = document.querySelector("#search-box");
 const countryList = document.querySelector("#country-list");
 const countryInfo = document.querySelector("#country-info");
 
+// Слухач подій
 input.addEventListener("input", debounce(onInput, 500));
 
+// Основна функція обробки вводу
 function onInput(e) {
-  const searchQuery = e.target.value.trim();
+  const query = e.target.value.trim();
 
-  if (!searchQuery) {
+  if (!query) {
     clearMarkup();
     return;
   }
 
-  fetchCountries(searchQuery)
+  fetchCountries(query)
     .then((data) => {
       clearMarkup();
 
@@ -40,13 +42,24 @@ function onInput(e) {
     });
 }
 
+// Запит до API
+function fetchCountries(name) {
+  const BASE_URL = "https://restcountries.com/v2/name";
+  return fetch(
+    `${BASE_URL}/${name}?fields=name,capital,population,languages,flag`
+  ).then((res) => {
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return res.json();
+  });
+}
 function renderCountryList(countries) {
   const markup = countries
     .map((country) => `<li class="country-item">${country.name}</li>`)
     .join("");
   countryList.innerHTML = markup;
 }
-
 function renderCountryInfo(country) {
   const languages = country.languages.map((lang) => lang.name).join(", ");
   const markup = `
@@ -58,7 +71,6 @@ function renderCountryInfo(country) {
   `;
   countryInfo.innerHTML = markup;
 }
-
 function clearMarkup() {
   countryList.innerHTML = "";
   countryInfo.innerHTML = "";
